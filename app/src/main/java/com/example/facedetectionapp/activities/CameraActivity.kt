a1058623ac7ed1d7cb0175e1a08953c892e36adc
Vishal Chand <sam.chand96@gmail.com>
2023-11-08 21:34:00 +0530

    Working on saving the preview with overlay, still in progress.

--- app/src/main/java/com/example/facedetectionapp/activities/CameraActivity.kt
+++ app/src/main/java/com/example/facedetectionapp/activities/CameraActivity.kt
@@ -1,11 +1,15 @@
 package com.example.facedetectionapp.activities
 
+import android.content.ContentValues
 import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.os.Environment
+import android.provider.MediaStore
 import android.util.Log
 import android.view.View
 import android.widget.Toast
@@ -34,6 +38,7 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.io.File
 import java.io.FileOutputStream
+import java.io.OutputStream
 import java.util.concurrent.Executors
 
 class CameraActivity : AppCompatActivity() {
@@ -182,19 +187,72 @@ class CameraActivity : AppCompatActivity() {
                 "${System.currentTimeMillis()}"
 
             val bitmap = getBitmapFromView(binding.cameraView)
-            storeBitmapLocally(bitmap, name)
+//            storeBitmapLocally(bitmap, name)
+            if (bitmap != null) {
+                saveMediaToStorage(bitmap)
+            }
         } else {
             requestStoragePermission()
         }
     }
 
-    private fun getBitmapFromView(view: View): Bitmap {
-        val bitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
-        val canvas = Canvas(bitmap)
-        view.draw(canvas)
-        return bitmap
+    private fun getBitmapFromView(view: View): Bitmap? {
+        var screenshot: Bitmap? = null
+        try {
+            screenshot = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+            val canvas = Canvas(screenshot)
+            view.draw(canvas)
+        }catch (e: Exception) {
+            Log.e("Bitmap can't be generated", e.toString())
+        }
+        return screenshot
     }
+    // this method saves the image to gallery
+    private fun saveMediaToStorage(bitmap: Bitmap) {
+        // Generating a file name
+        val filename = "${System.currentTimeMillis()}.jpg"
 
+        // Output stream
+        var fos: OutputStream? = null
+
+        // For devices running android >= Q
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            // getting the contentResolver
+            this.contentResolver?.also { resolver ->
+
+                // Content resolver will process the content-values
+                val contentValues = ContentValues().apply {
+
+                    // putting file information in content values
+                    put(MediaStore.MediaColumns.DISPLAY_NAME, filename)
+                    put(MediaStore.MediaColumns.MIME_TYPE, "image/jpg")
+                    put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
+                }
+
+                // Inserting the contentValues to
+                // contentResolver and getting the Uri
+                val imageUri: Uri? = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+
+                // Opening an outputstream with the Uri that we got
+                fos = imageUri?.let { resolver.openOutputStream(it) }
+            }
+        } else {
+            // These for devices running on android < Q
+            val imagesDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+            val image = File(imagesDir, filename)
+            fos = FileOutputStream(image)
+        }
+
+        fos?.use {
+            // Finally writing the bitmap to the output stream that we opened
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, it)
+            runOnUiThread {
+                Toast.makeText(this , "Captured View and saved to Gallery" , Toast.LENGTH_SHORT).show()
+            }
+        }
+        fos?.flush()
+        fos?.close()
+    }
     private fun storeBitmapLocally(bitmap: Bitmap, name: String) {
         CoroutineScope(Dispatchers.IO).launch {
             try {
